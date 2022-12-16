fc590e11a1c28369842c5a782ae7b5ef5eb2d7a1
Lucas Huerta <lucashuerta@macbook-pro-de-lucas.home>
2022-12-16 17:31:00 +0100

    Start home page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <ul>
+            <li>
+              Home
+            </li>
+            <li>
+              Projects
+            </li>
+            <li>
+              About
+            </li>
+          </ul>
+        </nav>
+        <section className="Section-Title">
+          <h1>
+            LUCAS
+          </h1>
+          <h1>
+            HUERTA
+          </h1>
+        </section>
       </header>
     </div>
   );
