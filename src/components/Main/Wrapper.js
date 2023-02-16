8381688e4668177468963ce0e51880c16420cd89
Huerta <lucas.huerta@edu.univ-fcomte.fr>
2023-02-16 14:21:49 +0100

    [ADD] Gsap timeline and fixed section in scroll

--- src/components/Main/Wrapper.js
+++ src/components/Main/Wrapper.js
@@ -1,45 +1,105 @@
 import gsap from "gsap"
-import React from "react";
-// import { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { ScrollTrigger } from "gsap/all"
 import { ScrollToPlugin } from "gsap/all"
 
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin); 
 
-export default class Wrapper extends React.Component {
-    componentDidMount() {
-      const timeline = gsap.timeline({
-        scrollTrigger: {
-            pin: true, 
-            scrub: 1,
-            trigger: ".mainProjects",
-            start: "top center",
-            end: "top bottom",
-            markers: true, 
+export default function Wrapper() {
+    const myElementRef = useRef(null);
+    const titleRef = useRef(null);
+
+    useEffect(() => {
+        const myElement = myElementRef.current;
+        const titleMyElement = titleRef.current
+
+        function first() {
+            var tl = gsap.timeline();
+            tl.from(".square1", 
+            { 
+                y: "-500%", ease: "power1.inOut", opacity: 0, yoyo: true
+            }, 0)
+            .to(".square1", 
+            { 
+                y: "50%", ease: "power1.inOut", opacity: 1, yoyo: true
+            }, 0)
+            return tl;
         }
-      });
-  
-      timeline
-        .to(".square1", 
+        
+        function second() {
+            var tl = gsap.timeline();
+            tl.from(".square2", 
             { 
-                y: "20%", ease: "ease-in-out", opacity: 1, yoyo: true
+                y: "-500%", ease: "power1.inOut", opacity: 0, yoyo: true
             }, 0)
-        .to(".square2", 
+            .to(".square2", 
             { 
-                y: "50%", ease: "ease-in-out", opacity: 1, yoyo: true
+                y: "150%", ease: "power1.inOut", opacity: 1, yoyo: true
             }, 0)
-        .to(".square3", 
+            return tl;
+        }
+        
+        function third() {
+            var tl = gsap.timeline();
+            tl.from(".square3", 
             { 
-                y: "100%", ease: "ease-in-out", opacity: 1, yoyo: true
+                y: "-500%", ease: "power1.inOut", opacity: 0, yoyo: true
             }, 0)
-        ;
-        timeline.reverse();
-    }
-  
-    render() {
+            .to(".square3", 
+            { 
+                y: "200%", ease: "power1.inOut", opacity: 1, yoyo: true
+            }, 0)
+            return tl;
+        }
+    
+        function handleScroll() {
+
+            const windowHeight = window.innerHeight;
+            const elementTop = myElement.getBoundingClientRect().top + windowHeight;
+
+            if(elementTop >= windowHeight){
+                titleMyElement.style.position = 'inherit';
+            }
+            else if (elementTop <= windowHeight) {
+                titleMyElement.style.position = 'fixed';
+                titleMyElement.style.top = "2px";
+                titleMyElement.style.left = "25%";
+                titleMyElement.style.right = "25%";
+                const tl = gsap.timeline({
+                    scrollTrigger: {
+                        pin: true, 
+                        scrub: 1,
+                        // pinType: "fixed",
+                        trigger: ".mainProjects",
+                        start: "bottom center",
+                        end: "bottom bottom",
+                        // onEnter: ({progress, direction, isActive}) => {
+                        //     console.log(progress, direction, isActive);
+                        //     // let main = document.getElementsByClassName('.mainProject');
+                        //     // main.classList.add('fixed');
+                        // },
+                        markers: true,
+                        // snap: 0.1
+                    }
+                });
+                tl.add(first(), "+=2")
+                    .add(second(), "+=3")     //with a gap of 2 seconds
+                    .add(third(), "+=4") //overlap by 1 second
+
+                return tl;
+            }
+        }
+    
+        window.addEventListener('scroll', handleScroll);
+    
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
+
       return (
-        <section id='main-Wrapper' className="wrapper">
-            <h2>
+        <section id='main-Wrapper' ref={myElementRef} className="wrapper">
+            <h2 ref={titleRef}>
                 Projects
             </h2>
             <div className="mainProjects">
@@ -49,5 +109,4 @@ export default class Wrapper extends React.Component {
             </div>
         </section>
       );
-    }
 }
